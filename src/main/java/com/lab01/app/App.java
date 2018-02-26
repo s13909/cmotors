d12755d25b581b5c24505e10fba7ed5663ff9878
s13909 <s13909@pjwstk.edu.pl>
2018-02-26 15:34:46 +0100

    second

---
+++ src/main/java/com/lab01/app/App.java
@@ -0,0 +1,13 @@
+package com.lab01.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worlds!" );
+    }
+}
