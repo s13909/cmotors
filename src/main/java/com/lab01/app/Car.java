719d596d16a46bcbff2cf6d566ee8b2a2b5799c5
jaruzzzelski <s13909@pjwstk.edu.pl>
2018-03-05 12:25:18 +0100

    fixing test

--- src/main/java/com/lab01/app/Car.java
+++ src/main/java/com/lab01/app/Car.java
@@ -1,14 +1,10 @@
 package com.lab01.app;
+
 public class Car 
 {
-    public static void main( String[] args )
-    {
-            int id;
-            String make;
-            String model;
-            String color;
-            int year;
-        
- 
-    }
+    int id;
+    String make;
+    String model;
+    String color;
+    int year;   
 }
